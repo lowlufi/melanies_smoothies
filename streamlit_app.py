da359f4cd57546f11fd5d3163a2e859d6d1ced20
Daniel Aguilera Campusano <50189054+lowlufi@users.noreply.github.com>
2025-04-12 14:45:24 -0400

    probando si era la gest active session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+from snowflake.snowpark.context import get_active_session
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Batido!:cup_with_straw:")
 st.write(
@@ -12,6 +12,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
