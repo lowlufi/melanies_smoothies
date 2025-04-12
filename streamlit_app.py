0a20b0db0ddeeac0978384bd9867b58a7d4387fe
Daniel Aguilera Campusano <50189054+lowlufi@users.noreply.github.com>
2025-04-12 18:47:55 -0400

    prueba3

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
         pd_df = st.dataframe(data=fruitvoice_response.json(), use_container_width=True)
 
         #st.write(ingredients_string)
-    time_to_insert = st.button('Submit Order')
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie is ordered!', icon="✔")
+        time_to_insert = st.button('Submit Order')
+            if time_to_insert:
+                session.sql(my_insert_stmt).collect()
+                st.success('Your Smoothie is ordered!', icon="✔")
