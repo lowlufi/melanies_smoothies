738a5074e027669343b42660cfad9494dd478ed2
Daniel Aguilera Campusano <50189054+lowlufi@users.noreply.github.com>
2025-04-12 18:39:40 -0400

    prueba3

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,27 +30,27 @@ except Exception as e:
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
-    max_selections=6
+    max_selections=5
 )
 
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruitvoice_response = requests.get("https://fruitvoice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruitvoice_response.json(), use_container_width=True)
+    ingredients_string += fruit_chosen + ''
 
-#st.write(ingredients_string)y
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-    time_to_insert = st.button('Submit Order') 
+    st.subheader(fruit_chosen + ' Nutrition Information')
+    fruitvoice_response = requests.get("https://fruitvoice.com/api/fruit/" + search_on)
+    fv_df = st.dataframe(data=fruitvoice_response.json(), use_container_width=True)
+
+#st.write(ingredients_string)
+
+    time_to_insert = st.button('Submit Order')
+
     if time_to_insert:
-        try:
-            session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie is ordered!', icon="✔")
-        except Exception as e:
-            st.error(f"Error submitting order: {str(e)}")
-            st.text(my_insert_stmt)  # Mostrar la consulta para depuración
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✔")
+        
